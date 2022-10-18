867ef8a3bcad49efa812d8dc7dcb6e1ba78886b9
Khanim Humbatova <khumbatova@pgh.yearup.org>
2022-10-18 09:15:33 -0400

    add math.js

---
+++ exercise-1.js
@@ -0,0 +1,7 @@
+let customerId = "" ;
+let firstName = "Khanim";
+let middleName = null;
+let lastName ="Humbatova";
+let customerDOB ="22.07.1996";
+let driverLicenseNum = null;
+let autopolicyNum = null;
---
+++ math.js
@@ -0,0 +1,7 @@
+
+let tacoPrice = 6.00;
+console.log(typeof tacoPrice)
+let cupon = 3.00;
+let tacoQuantity = 3;
+let totalTacoPrice = tacoPrice * tacoQuantity;
+console.log(totalTacoPrice);
